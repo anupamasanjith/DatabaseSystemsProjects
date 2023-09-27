64477d4fd1f3d745accd9a3e9656a299929cc7ef
anupamasanjith <sanjithanupama@gmail.com>
2023-09-27 11:23:52 -0400

    exam1

---
+++ exam1.sql
@@ -0,0 +1,70 @@
+-- Exam 1
+-- Question 1
+-- Write a query that generates a table of actors as and the movies they acted in.
+-- row contains (actor name,movie title. release year,director, and character name
+-- ordered by last name, release year.
+
+SELECT
+    actors.firstname,actors.lastname,
+    movies.title,movies.releaseyear,movies.director,
+    actsin.charactername
+FROM
+    actors,movies,actsin
+WHERE
+    actors.actorid = actsin.actorid AND
+    movies.movieid = actsin.movieid
+ORDER BY
+    actors.lastname,movies.releaseyear;
+
+
+--Question 2
+-- Query that displays names of the actors who have acted in most number of movies
+-- output schema (lastname,actorid,count)
+
+SELECT
+    lastname,actorid,count
+FROM(SELECT
+    lastname,actorid,count(*) as count
+     FROM
+         (SELECT
+              actors.firstname,
+              actors.lastname,
+              movies.title,
+              actors.actorid
+         FROM
+             actors,
+             movies,
+             actsin
+         WHERE
+             actors.actorid = actsin.actorid AND
+             movies.movieid = actsin.movieid) AS t
+     GROUP BY firstname,t.lastname, actorid) AS sub
+
+WHERE
+    count = (SELECT max(count)
+             FROM
+                 (SELECT
+                     count(*) as count
+                  FROM
+                      (SELECT
+                           actors.firstname,
+                           actors.lastname,
+                           movies.title,
+                           actors.actorid
+                       FROM
+                           actors,
+                           movies,
+                           actsin
+                       WHERE
+                           actors.actorid = actsin.actorid AND
+                           movies.movieid = actsin.movieid) AS t2
+                  GROUP BY
+                      lastname,actorid) as subtwo);
+
+
+
+
+\
+
+
+
